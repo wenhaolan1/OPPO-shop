6f770211c9b52e43308003d039efb14dee7ca7b1
兰文昊 <1357540403@QQ.com>
2022-07-17 20:38:42 +0800

    opposhop

--- src/api/request.js
+++ src/api/request.js
@@ -2,10 +2,13 @@ import axios from 'axios'
 
 
 export const getBanners = () => 
-    axios.get('https://www.fastmock.site/mock/b382251bce55be951b9acb71a1348802/beers/api/request')
+    axios.get('https://www.fastmock.site/mock/b382251bce55be951b9acb71a1348802/beers/api/request/banners')
 
 export const getGoods = () => 
     axios.get('https://www.fastmock.site/mock/b382251bce55be951b9acb71a1348802/beers/api/request/Goods')
 
 export const getGoodslist = () => 
     axios.get('https://www.fastmock.site/mock/b382251bce55be951b9acb71a1348802/beers/api/request/Goodslist')
+
+export const getClassifyListRequest = () =>
+    axios.get('https://www.fastmock.site/mock/b382251bce55be951b9acb71a1348802/beers/api/request/classify')
